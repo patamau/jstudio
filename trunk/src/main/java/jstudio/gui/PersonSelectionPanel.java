8f53f0a09b9a77831da3ebc457d8a651300c6d20
patamau@gmail.com <patamau@gmail.com@6d2ef6ab-18e7-7421-15d5-f74056d88645>
2011-09-05 08:32:54 +0000

    fixed accouting column sizes fixed person selection

--- trunk/src/main/java/jstudio/gui/PersonSelectionPanel.java
+++ trunk/src/main/java/jstudio/gui/PersonSelectionPanel.java
@@ -1,28 +1,144 @@
 package jstudio.gui;
 
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Frame;
 import java.awt.Window;
+import java.awt.event.ActionEvent;
 import java.awt.event.MouseEvent;
+import java.util.Collection;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.JToolBar;
+import javax.swing.ListSelectionModel;
 import javax.swing.SwingUtilities;
 
 import jstudio.control.Controller;
+import jstudio.gui.generic.EntityManagerPanel;
+import jstudio.gui.generic.PopupListener;
 import jstudio.model.Person;
+import jstudio.util.Language;
+import jstudio.util.Resources;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SuppressWarnings("serial")
-public class PersonSelectionPanel extends AddressBookPanel {
+public class PersonSelectionPanel extends EntityManagerPanel<Person> {
+
+	private static final Logger logger = LoggerFactory.getLogger(AddressBookPanel.class);
+
+	public static final String PIC_ADDRESSBOOK="personicon.png";
 
 	private Person selectedPerson;
+	private JButton refreshButton;
 	
-	public PersonSelectionPanel(Controller<Person> controller) {
+	public PersonSelectionPanel(Controller<Person> controller){
 		super(controller);
+		this.setLayout(new BorderLayout());
+		
+		table = new JTable();
+		model = new PersonSelectionTableModel(table);
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+		JScrollPane scrollpane = new JScrollPane(table);
+		//scrollpane.setPreferredSize(new Dimension(this.getWidth(),this.getHeight()));
+		this.add(scrollpane, BorderLayout.CENTER);
+
+		JToolBar actionPanel = new JToolBar(Language.string("Actions"));
+		actionPanel.setFloatable(false);
+		refreshButton = new JButton(Language.string("Refresh"));
+		refreshButton.addActionListener(this);
+		refreshButton.setPreferredSize(new Dimension(60,25));
+		actionPanel.add(refreshButton);
+		filterField = new JTextField();
+		filterField.addKeyListener(this);
+		filterField.setPreferredSize(new Dimension(0,25));
+		actionPanel.add(filterField);
+		actionPanel.setPreferredSize(new Dimension(0,25));
+		this.add(actionPanel, BorderLayout.NORTH);
+		
+		scrollpane.addMouseListener(this);
+		table.addMouseListener(this);
+		this.popup = new PersonPopup(this, controller);
+	    table.addMouseListener(new PopupListener<Person>(table, super.popup));
 	}
 	
 	public Person getSelected(){
 		return selectedPerson;
 	}
 	
-	public void mouseClicked(MouseEvent e){
+	public String getLabel(){
+		return Language.string("Address book");
+	}
+	
+	public ImageIcon getIcon(){
+		return Resources.getImage(PIC_ADDRESSBOOK);
+	}
+	
+	public void showEntity(Person p){
+		JDialog dialog = new PersonPanel(p,this, false).createDialog((Frame)this.getTopLevelAncestor());
+		dialog.setVisible(true);
+	}
+
+	public synchronized void addEntity(Person p){
+		model.addRow(new Object[]{
+				p,
+				p.getName(),
+				p.getBirthdate(),
+				p.getCity()});
+	}
+	
+	public void filter(String text){
+		text = text.trim();
+		this.clear();
+		String[] vals = text.split(" ");
+		String[] cols = new String[]{
+				"name",
+				"lastname" };
+		Collection<Person> ts = controller.findAll(vals, cols);
+		if(ts!=null){
+			for(Person t: ts){
+				this.addEntity(t);
+			}
+		}else{
+			JOptionPane.showMessageDialog(this, Language.string("Unable to load data"),Language.string("Database error"),JOptionPane.ERROR_MESSAGE);
+		}
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		Object o = e.getSource();
+		if(o==refreshButton){
+			refresh();
+		} else {
+			logger.warn("Event source not mapped: "+o);
+		}
+	}
+	
+	public void showDialog(Component parent){
+		this.showDialog(parent, null);
+	}
+	
+	public void showDialog(Component parent, String filterText){
+		JDialog dialog = new JDialog();
+		dialog.setTitle(Language.string("Select Person"));
+		dialog.setModal(true);
+		dialog.add(this);
+		this.filterField.setText(filterText);
+		this.filter(filterText);
+		dialog.pack();
+		dialog.setLocationRelativeTo(parent);
+		dialog.setVisible(true);
+	}
+	
+	public void mouseClicked(MouseEvent e){ 
 		int row = table.rowAtPoint(e.getPoint());
 		if(row>=0){
 			selectedPerson = (Person)table.getValueAt(row, 0);
@@ -32,23 +148,9 @@ public class PersonSelectionPanel extends AddressBookPanel {
 			}
 		}
 	}
-
-	public void mouseReleased(MouseEvent e){
-		//override default behavior with the empty one
-	}
 	
-	public void showDialog(String filterText){
-		JDialog ppdialog = new JDialog();
-		ppdialog.setModal(true);
-		if(filterText==null){
-			refresh();
-		}else{
-			this.filterField.setText(filterText);
-			filter(filterText);
-		}
-		ppdialog.add(this);
-		ppdialog.pack();
-		ppdialog.setLocationRelativeTo(this);
-		ppdialog.setVisible(true);
-	}
+	/**
+	 * Intentionally empty to avoid popup to show
+	 */
+	public void mouseReleased(final MouseEvent e){ }
 }
--- trunk/src/main/java/jstudio/gui/PersonSelectionTableModel.java
+++ trunk/src/main/java/jstudio/gui/PersonSelectionTableModel.java
@@ -5,28 +5,24 @@ import java.util.Date;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
-import jstudio.model.Invoice;
 import jstudio.util.Language;
 import jstudio.util.TableSorter;
 
 @SuppressWarnings("serial")
-public class AccountingTableModel extends DefaultTableModel {
+public class PersonSelectionTableModel extends DefaultTableModel {
 	
 	private static final Object[] cols = new Object[]{
-			Language.string("Id"),
-			Language.string("Date"), 
-			Language.string("Contact"), 
-			Language.string("Description"),
-			Language.string("Total"),
+			Language.string("Lastname"), 
+			Language.string("Name"), 
+			Language.string("Birthdate"),
+			Language.string("City"),
 			};
 	
-	public AccountingTableModel(JTable table) {
+	public PersonSelectionTableModel(JTable table) {
 		super(cols, 0);
 		TableSorter ts = new TableSorter(this,table.getTableHeader());
 		table.setModel(ts);
-		ts.setSortingStatus(0, TableSorter.ASCENDING);
-		table.getColumn("Id").setMaxWidth(50);
-		table.getColumn("Id").setMinWidth(50);
+		ts.setSortingStatus(0, TableSorter.DESCENDING);
 	}
 
 	public boolean isCellEditable(int row, int col){
@@ -35,9 +31,7 @@ public class AccountingTableModel extends DefaultTableModel {
 	
 	public Class<?> getColumnClass(int col){
 		switch(col){
-		case 0: return Invoice.class;
-		case 1: return Date.class;
-		case 4: return Float.class;
+		case 2: return Date.class;
 		default: return String.class;
 		}
 	}
