f30a589538c2d001d05351d1d5a3e81fcbeea25c
patamau@gmail.com <patamau@gmail.com@6d2ef6ab-18e7-7421-15d5-f74056d88645>
2012-03-23 22:28:46 +0000

    fixed invoice related guis

--- trunk/src/main/java/jstudio/gui/ProductTableModel.java
+++ trunk/src/main/java/jstudio/gui/ProductTableModel.java
@@ -9,21 +9,23 @@ import jstudio.util.Language;
 @SuppressWarnings("serial")
 public class ProductTableModel extends DefaultTableModel {
 	
-	private static final Object[] cols = new Object[]{
+	private static final Object[] cols = new Object[]{"","",""};
+	/*
 			Language.string("Description"), 
 			Language.string("Quantity"), 
 			Language.string("Cost")
 			};
+	*/
 	
 	public ProductTableModel(JTable table, Invoice invoice) {
 		super(cols, 0);
 		table.setModel(this);
-		table.getColumn(table.getColumnName(1)).setMaxWidth(50);
-		table.getColumn(table.getColumnName(1)).setMinWidth(50);
-		table.getColumn(table.getColumnName(2)).setMaxWidth(100);
-		table.getColumn(table.getColumnName(2)).setPreferredWidth(100);
-		table.getColumn(table.getColumnName(2)).setMinWidth(50);
-		table.getColumn(table.getColumnName(0)).setPreferredWidth(150);
+		table.getColumnModel().getColumn(1).setMaxWidth(50);
+		table.getColumnModel().getColumn(1).setMinWidth(50);
+		table.getColumnModel().getColumn(2).setMaxWidth(100);
+		table.getColumnModel().getColumn(2).setPreferredWidth(100);
+		table.getColumnModel().getColumn(2).setMinWidth(50);
+		table.getColumnModel().getColumn(0).setPreferredWidth(150);
 	}
 	
 	public boolean isCellEditable(int row, int col){
